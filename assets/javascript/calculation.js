712719edb821eeb137a702801c357a402dcd56c1
King Conch <kbmajor80@gmail.com>
2019-04-13 01:36:23 -0400

    ...

--- assets/javascript/calculation.js
+++ assets/javascript/calculation.js
@@ -1,7 +1,11 @@
-var caloriesBurned =;
-var maleCalc = (4.184 * caloriesBurned) / ((maleAge * .2017) - (maleWeight * .09036) + (maleHR * .6309) - 55.0969);
+var caloriesBurned = $('#calories').text().val().trim();
+
+// var maleCalc = (4.184 * caloriesBurned) / ((maleAge * .2017) - (maleWeight * .09036) + (maleHR * .6309) - 55.0969);
 
 
-jogging = 100;
-swimming = 290;
-biking = 509
+// jogging = 100;
+// swimming = 290;
+// biking = 509;
+
+
+console.log(caloriesBurned);
