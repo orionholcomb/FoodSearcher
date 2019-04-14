dc498479b00c5faa6f515c31d5aee80a918cfa35
King Conch <kbmajor80@gmail.com>
2019-04-13 20:12:42 -0400

    wip

--- assets/javascript/search.js
+++ assets/javascript/search.js
@@ -3,34 +3,110 @@ var searchFood;
 // Might want to use this wiht typeOf var stringValue = "";
 
 
-    $("#runSearch").on("click", function(event) {
+var displayResults = function(data) {
+    
+    $('#servId').text(data.hits[0].fields.item_name);
+    $("#calories").text("Calories: " + data.hits[0].fields.nf_calories + " kcal");
+    $("#carbs").text("Carbohydrates: " + data.hits[0].fields.nf_total_carbohydrate + " g");
+    $("#protein").text("Proteins: " + data.hits[0].fields.nf_protein + " g");
+    $("#sodium").text("Sodium: " + data.hits[0].fields.nf_sodium + " mg");
+    
+  };
 
-        /*This line 13 allows us to take advantage of the HTML "submit" property
+$("#runSearch").on("click", function(event) {
+  /*This line 13 allows us to take advantage of the HTML "submit" property
         This way we can hit enter on the keyboard and it registers the search
         (in addition to clicks). Prevents the page from reloading on form submit.*/
 
+  event.preventDefault();
 
-        event.preventDefault();
+  searchFood = $("#mealText").val().trim();
+
+  console.log(searchFood);
+
+  // The display Results function takes data returned by nutritionix.com and displays to the HTML body
+
+  var queryURL =
+    "https://api.nutritionix.com/v1_1/search/" +
+    searchFood +
+    "?results=0:1&fields=item_name,nf_calories,nf_total_carbohydrate,nf_protein,nf_sodium&appId=30e01e04&appKey=bf1283fb7b11b7f059bb34d69a04566c";
+
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+    console.log(response);
+    //console.log(response.hits[0].fields.nf_calories);
+
+    displayResults(response);
+
+    
+    var nfCal = response.hits[0].fields.nf_calories;
+
+$(".exerBtn").on("click", timeCalc);
+
+function timeCalc() {
+  var exerciseCalc = $(this).attr("data-name");
+  var weight = $("#userWeight").val().trim();
+
+  if (exerciseCalc === "jog") {
+    var met = 6;
+  } else if (exerciseCalc === "swim") {
+    var met = 6;
+  } else if (exerciseCalc === "bike") {
+    var met = 8;
+  }
+
+  console.log(this);
+  console.log(exerciseCalc);
+  console.log(nfCal);
+  console.log(weight);
 
 
-        searchFood = $('#mealText').val().trim();
+ 
+  var returnCalc =
+    (nfCal) / ((met * parseInt(weight)) / 2.2);
 
-        console.log(searchFood);
+  $("#gamePlan").text(returnCalc);
 
-        var queryURL = 
-        "https://api.nutritionix.com/v1_1/search/" + searchFood + "?results=0:1&fields=item_name,nf_calories,nf_calories_from_fat&appId=30e01e04&appKey=bf1283fb7b11b7f059bb34d69a04566c";
+}
 
-        $.ajax({
-            url: queryURL,
-            method: "GET",
-          }).then (function(response){
+    
 
-              console.log(response);
-              console.log(response.hits[0].fields.nf_calories);
-              
-              $('#calories').text(response.hits[0].fields.nf_calories);
 
-          });
+    $("#searchResults").text(response.hits[0].fields.nf_calories);
 
-          
-        });
+  })
+});
+
+//displayResults();
+
+// $(".exerBtn").on("click", timeCalc);
+
+// function timeCalc() {
+//   var exerciseCalc = $(this).attr("data-name");
+
+//   if (exerciseCalc === "jog") {
+//     var met = 6;
+//   } else if (exerciseCalc === "swim") {
+//     var met = 6;
+//   } else if (exerciseCalc === "bike") {
+//     var met = 8;
+//   }
+
+//   console.log(this);
+//   console.log(exerciseCalc);
+
+//   var caloriesBurned = $("#searchResults")
+//     .val()
+//     .trim();
+
+//   var returnCalc =
+//     (parseInt(caloriesBurned) / ((met * parseInt(maleWeight)) / 2.2));
+
+//   $("#gamePlan").text(returnCalc);
+
+//   console.log(caloriesBurned);
+//   console.log(maleWeight);
+//   console.log(met);
+// }
