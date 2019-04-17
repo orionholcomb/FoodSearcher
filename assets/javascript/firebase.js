c00e08b4d251e8fcd48bac9330b5d8a078eff203
OrionHolcomb <orionholcomb@gmail.com>
2019-04-16 20:26:28 -0400

    made an on click function to store FB data

--- assets/javascript/firebase.js
+++ assets/javascript/firebase.js
@@ -1,4 +1,35 @@
-// Variables that store user inputs
-var weight = $('#userWeight').val().trim();
-var zipcode = $('#zipcode').val().trim();
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyDCbDyvb7466TQ92Zz62wkeenxxwnBAfJg",
+  authDomain: "food-searcher-2148b.firebaseapp.com",
+  databaseURL: "https://food-searcher-2148b.firebaseio.com",
+  projectId: "food-searcher-2148b",
+  storageBucket: "food-searcher-2148b.appspot.com",
+  messagingSenderId: "269446386428"
+};
+firebase.initializeApp(config);
 
+// Reference for the firebase database
+var database = firebase.database();
+
+// On click function that stores our variables in firebase
+$(".exerBtn").on("click", function(event) {
+  event.preventDefault();
+
+  // Variables that store user inputs
+  var username = $("#userName")
+    .val()
+    .trim();
+  var weight = $("#userWeight")
+    .val()
+    .trim();
+
+  database.ref().set({
+    nameInput: username,
+    weightInput: weight
+  });
+});
+
+database.ref().on("value", function(snapshot) {
+  console.log(snapshot.val());
+});
